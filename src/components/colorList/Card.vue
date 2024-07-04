ae7beb786ef475d156e472acb5d9d2bc7a92a7bf
oldflood <hhq@moreyou.cn>
2024-07-04 14:13:17 +0800

    adjust menu

--- src/components/colorList/Card.vue
+++ src/components/colorList/Card.vue
@@ -20,9 +20,11 @@ const onClick = () => {
   <div
       class="color-card"
       @click="onClick"
-      @mouseenter="cursorStore.setCursor('large',color.name)"
+      @mouseenter="cursorStore.setCursor('small',color.name)"
   >
-    <div class="color-card-swatch" :style="{ background: color.hex}"></div>
+    <div class="color-card-swatch" :style="{ background: color.hex}">
+      <div class="color-sentence">{{ color.sentence }}</div>
+    </div>
     <div class="color-card-desc">
       <div class="title">{{ color.name }}</div>
       <div class="hex">{{ color.hex }}</div>
@@ -41,17 +43,39 @@ const onClick = () => {
   transition: all ease-in .5s;
   cursor: pointer;
 
-  &:hover {
-    box-shadow: 0 8px 12px -4px rgba(184, 184, 210, 0.20);
-  }
 
   .color-card-swatch {
+    display: flex;
+    justify-content: flex-end;
+    padding: 8px;
     width: 100%;
     aspect-ratio: 1;
     border-radius: 6px;
+    transition: all ease-in .3s;
+
+    .color-sentence {
+      color: #fff;
+      font-size: 12px;
+      line-height: 1.5;
+      writing-mode: vertical-rl;
+      mix-blend-mode: overlay;
+      opacity: 0;
+      transition: all ease-in .3s;
+    }
+  }
+
+  &:hover {
+    box-shadow: 0 8px 12px -4px rgba(184, 184, 210, 0.20);
+    transform: scale(1.4);
+
+    .color-sentence {
+      opacity: 1;
+    }
+
   }
 
   .color-card-desc {
+    font-size: 12px;
     div {
       margin: 8px 0;
     }
